2f38b25578d1b79fd5d8f39f19123fb1a87676f5
Arnav Arora <arnavarora@Arnavs-MacBook-Air.local>
2024-03-14 02:01:45 +0530

    Pro details screen

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -3,70 +3,70 @@ const products = [
     id: 1,
     name: 'Bread',
     image:
-      'https://i.imgur.com/dDIDpwq.png',
+      'https://i.imgur.com/8DnwWy2.png',
     price: 50.00,
   },
   {
     id: 2,
     name: 'Black Forest Cake',
     image:
-      'https://i.imgur.com/FTLEdf6.jpeg',
+      'https://i.imgur.com/VlcTAtp.png',
     price: 450.00,
   },
   {
     id: 3,
     name: 'Bun',
     image:
-      'https://i.imgur.com/Y79qG50.jpeg',
+      'https://i.imgur.com/EEClnxe.png',
     price: 35.00,
   },
   {
     id: 4,
     name: 'Biscoff Cheesecake',
     image:
-      'https://i.imgur.com/TDbgpNL.png',
+      'https://i.imgur.com/ojlvMCu.png',
     price: 320.00,
   },
   {
     id: 5,
     name: 'Grilled Cheese Toast',
     image:
-      'https://i.imgur.com/tJno1yD.jpeg',
+      'https://i.imgur.com/8JOkBoV.png',
     price: 65.00,
   },
   {
     id: 6,
     name: 'Choco Chip Cake',
     image:
-      'https://i.imgur.com/5RDuIoQ.jpeg',
+      'https://i.imgur.com/i6rhhYi.png',
     price: 750.00,
   },
   {
     id: 7,
     name: 'Vanilla Creamroll',
     image:
-      'https://i.imgur.com/EaTm8xI.png',
+      'https://i.imgur.com/7imj1mm.png',
     price: 30.00,
   },
   {
     id: 8,
     name: 'Veg Toast',
     image:
-      'https://i.imgur.com/6zeAjp1.jpeg',
+      'https://i.imgur.com/LkWV1w3.png',
     price: 65.00,
   },
   {
     id: 9,
     name: 'Cappuccino',
     image:
-      'https://i.imgur.com/gIKMrMf.png',
+      'https://i.imgur.com/gQrPsrM.png',
     price: 20.00,
   },
   {
     id: 10,
     name: 'Tea',
     image:
-      'https://i.imgur.com/pPDzKhW.png',
+      'https://i.imgur.com/0Iz7o2f.png',
     price: 25.00,
   },
 ];
